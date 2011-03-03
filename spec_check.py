7d73433d12671be5a10756289f019271a4d66107
Jeffrey Ness <jness@flip-edesign.com>
2011-03-03 14:11:51 -0600

    very very very hacky....

--- spec_check.py
+++ spec_check.py
@@ -2,8 +2,11 @@
 
 from glob import glob
 from configobj import ConfigObj
+import subprocess
 import sys, os
 import argparse
+import urllib
+import shutil, shlex
 
 class colors:
     pink = '\033[95m'
@@ -68,23 +71,70 @@ def speclicense(spec_lines):
             license = line
             return license
 
+def specbuildrequires(spec_lines):
+    for line in spec_lines:
+        if 'BuildRequires: ' in line:
+            print line
+        
+
+def runcommand(command):
+    args = shlex.split(command)
+    process = subprocess.Popen(args, shell=False)
+    results = process.communicate()
+    results = results[0]
+    return results
+
 # Use argparse to take input for SPEC File
 parser = argparse.ArgumentParser()
-parser.add_argument('--spec', help='SPEC File', required=True)
+#parser.add_argument('--spec', help='SPEC File', required=True)
+parser.add_argument('--srpm', help='URL For SRPM', required=True)
 args = parser.parse_args()
 
 # Prep for run
 passed = []
 failed = []
-checks = configs()
-spec_lines = readspec(args.spec)
+checks = configs() # Read in all our checks
+path = './tmp' # Where SRPM is kept
+srpm = args.srpm.split('/')[-1]
+
+# Download the SRPM
+if not os.path.exists(path):
+    os.mkdir(path)
+    print 'Downloading', srpm
+    urllib.urlretrieve(args.srpm, path + '/' + srpm)
+
+# Check for .srpm
+if os.path.exists('./tmp/' + srpm):
+    print 'Installing', srpm
+    os.chdir(path)
+    install = runcommand('rpm -i --quiet ' + srpm)
+
+# Get our SPEC Name
+specs = glob('SPECS/*.spec')
+if len(specs) == 1:
+    spec = specs[0]
+else:
+    print 'Maybe the URL was invalid, maybe there is more than one SPEC...'
+    sys.exit(0)
+
+# Look for tarballs in SOURCE
+# FIX ME, i need some sort of looping
+tgz = glob('SOURCES/*.tar.gz')
+if tgz:
+    source = tgz
+tar = glob('SOURCES/*.tar.gz')
+if tar:
+    source = tar
+
+# Run through the spec
+spec_lines = readspec(spec)
 
 # Run our Checks
 for check in sorted(checks):
     os.system('clear')
     message = checks[check]['message']
     print 'CHECK: ' + str(check)
-    print specfile(args.spec)
+    print specfile(spec)
     print '='*80
     print '         ' + message + '\n'
     
@@ -94,9 +144,13 @@ for check in sorted(checks):
     except KeyError:
         pass
     else:
-        print specfile(args.spec) + ':'
+        print specfile(spec) + ':'
         print '-'*35
-        print '   ' + eval(command) + '\n'
+        commandoutput = eval(command)
+        if commandoutput:
+            print '   ' + commandoutput + '\n'
+        else:
+            print '\n'
 
     # See if we have Fedora Documentation
     try:
